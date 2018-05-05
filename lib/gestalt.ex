727ddd9c8cff9ae40f557a87247e1e387f1c4fd8
Eric Saxby <sax@livinginthepast.org>
2018-05-05 11:38:15 -0700

    Provides get_env and replace_env for wrapping System.get_env

--- lib/gestalt.ex
+++ lib/gestalt.ex
@@ -15,11 +15,22 @@ defmodule Gestalt do
   def get_config(module, key, pid, agent) when is_pid(pid) do
     case GenServer.whereis(agent) do
       nil -> Application.get_env(module, key)
-      _ -> get(agent, pid, module, key)
+      _ -> get_agent_config(agent, pid, module, key)
     end
   end
 
   def get_config(_module, _key, _pid, _agent), do: raise("get_config/3 must receive a pid")
+
+  def get_env(_varibale, _pid, _agent \\ __MODULE__)
+
+  def get_env(variable, pid, agent) when is_pid(pid) do
+    case GenServer.whereis(agent) do
+      nil -> System.get_env(variable)
+      _ -> get_agent_env(agent, pid, variable)
+    end
+  end
+
+  def get_env(_variable, _pid, _agent), do: raise("get_env/2 must receive a pid")
 
   ##############################
   ## Modify state
@@ -34,22 +45,47 @@ defmodule Gestalt do
 
       _ ->
         Agent.update(agent, fn state ->
-          Map.put(state, pid, %{module => %{key => value}})
+          Map.put(state, pid, configuration: %{module => %{key => value}})
         end)
     end
   end
 
   def replace_config(_module, _key, _value, _pid, _agent), do: raise("replace_config/4 must receive a pid")
 
+  def replace_env(_varibale, _value, _pid, _agent \\ __MODULE__)
+
+  def replace_env(variable, value, pid, agent) when is_pid(pid) do
+    case GenServer.whereis(agent) do
+      nil ->
+        raise "agent not started, please call start() before changing state"
+
+      _ ->
+        Agent.update(agent, fn state ->
+          Map.put(state, pid, env: %{variable => value})
+        end)
+    end
+  end
+
+  def replace_env(_variable, _value, _pid, _agent), do: raise("replace_env/3 must receive a pid")
+
   ##############################
   ## Private
   ##############################
 
-  defp get(agent, caller_pid, module, key) do
+  defp get_agent_config(agent, caller_pid, module, key) do
     Agent.get(agent, fn state ->
       case state[caller_pid] do
         nil -> Application.get_env(module, key)
-        _ -> get_in(state, [caller_pid, module, key])
+        _ -> get_in(state, [caller_pid, :configuration, module, key])
+      end
+    end)
+  end
+
+  defp get_agent_env(agent, caller_pid, variable) when is_binary(variable) do
+    Agent.get(agent, fn state ->
+      case state[caller_pid] do
+        nil -> System.get_env(variable)
+        _ -> get_in(state, [caller_pid, :env, variable])
       end
     end)
   end
--- test/gestalt_test.exs
+++ test/gestalt_test.exs
@@ -41,7 +41,7 @@ defmodule GestaltTest do
       end
     end
 
-    test "finds values from Application if the agent is not running" do
+    test "uses Application if the agent is not running" do
       refute GenServer.whereis(:not_running)
 
       assert Gestalt.get_config(:gestalt, :boolean_true, self(), :not_running) == true
@@ -56,7 +56,7 @@ defmodule GestaltTest do
                ]
     end
 
-    test "finds values from Application if the agent is running but there is no pid override" do
+    test "uses Application if the agent is running but there is no pid override" do
       {:ok, agent} = Gestalt.start(:get_config_value_no_override)
 
       assert Gestalt.get_config(:gestalt, :boolean_true, self(), :get_config_value_no_override) == true
@@ -73,12 +73,12 @@ defmodule GestaltTest do
       Agent.stop(agent)
     end
 
-    test "retrieves configuration from the running agent if there is a pid override" do
+    test "uses the running agent if there is a pid override" do
       pid = self()
       {:ok, agent} = Gestalt.start(:get_config_value_with_override)
 
       Agent.update(:get_config_value_with_override, fn state ->
-        Map.put(state, pid, %{module: %{key: "value"}})
+        Map.put(state, pid, configuration: %{module: %{key: "value"}})
       end)
 
       assert Gestalt.get_config(:module, :key, pid, :get_config_value_with_override) == "value"
@@ -86,15 +86,73 @@ defmodule GestaltTest do
       Agent.stop(agent)
     end
 
-    test "does not find any values from Application config if override is set" do
+    test "does not use Application if agent override is set" do
       pid = self()
       {:ok, agent} = Gestalt.start(:get_config_value_with_override)
 
       Agent.update(:get_config_value_with_override, fn state ->
-        Map.put(state, pid, %{module: %{key: "value"}})
+        Map.put(state, pid, configuration: %{module: %{key: "value"}})
       end)
 
       assert Gestalt.get_config(:gestalt, :boolean_true, pid, :get_config_value_with_override) == nil
+
+      Agent.stop(agent)
+    end
+  end
+
+  describe "get_env/2" do
+    test "raises when pid is not passed" do
+      assert_raise RuntimeError, "get_env/2 must receive a pid", fn ->
+        Gestalt.get_env("VARIABLE", nil)
+      end
+
+      assert_raise RuntimeError, "get_env/2 must receive a pid", fn ->
+        Gestalt.get_env("VARIABLE", "self()")
+      end
+    end
+
+    test "uses System if the agent is not running" do
+      refute GenServer.whereis(:not_running)
+
+      System.put_env("VARIABLE_AGENT_NOT_RUNNING", "exists")
+      assert Gestalt.get_env("VARIABLE_AGENT_NOT_RUNNING", self(), :not_running) == "exists"
+    end
+
+    test "uses System if the agent is running but there is no pid override" do
+      {:ok, agent} = Gestalt.start(:get_env_value_no_override)
+
+      System.put_env("VARIABLE_AGENT_RUNNING", "definitely exists")
+      assert Gestalt.get_env("VARIABLE_AGENT_RUNNING", self(), :not_running) == "definitely exists"
+
+      Agent.stop(agent)
+    end
+
+    test "uses the running agent if there is a pid override" do
+      pid = self()
+      {:ok, agent} = Gestalt.start(:get_env_value_with_override)
+
+      System.put_env("VARIABLE_OVERRIDE", "i am from the system")
+
+      Agent.update(:get_env_value_with_override, fn state ->
+        Map.put(state, pid, env: %{"VARIABLE_OVERRIDE" => "i am overridden"})
+      end)
+
+      assert Gestalt.get_env("VARIABLE_OVERRIDE", pid, :get_env_value_with_override) == "i am overridden"
+
+      Agent.stop(agent)
+    end
+
+    test "does not use System if agent override is set" do
+      pid = self()
+      {:ok, agent} = Gestalt.start(:get_env_value_with_override)
+
+      System.put_env("VARIABLE_OVERRIDE", "i am from the system")
+
+      Agent.update(:get_env_value_with_override, fn state ->
+        Map.put(state, pid, env: %{"OTHER_VARIABLE_OVERRIDE" => "i am overridden"})
+      end)
+
+      assert Gestalt.get_env("VARIABLE_OVERRIDE", pid, :get_env_value_with_override) == nil
 
       Agent.stop(agent)
     end
@@ -121,7 +179,34 @@ defmodule GestaltTest do
 
       assert Agent.get(:replace_config, fn state -> state end) == %{}
       Gestalt.replace_config(:gestalt, :key, true, pid, :replace_config)
-      assert Agent.get(:replace_config, fn state -> state end) == %{pid => %{gestalt: %{key: true}}}
+      assert Agent.get(:replace_config, fn state -> state end) == %{pid => [configuration: %{gestalt: %{key: true}}]}
+
+      Agent.stop(agent)
+    end
+  end
+
+  describe "replace_env/3" do
+    test "raises when pid is not passed" do
+      assert_raise RuntimeError, "replace_env/3 must receive a pid", fn ->
+        Gestalt.replace_env("SOME_VARIABLE", true, "self()")
+      end
+    end
+
+    test "raises when no agent has been started" do
+      refute GenServer.whereis(:replace_env_no_agent)
+
+      assert_raise RuntimeError, "agent not started, please call start() before changing state", fn ->
+        Gestalt.replace_env("SOME_VARIABLE", true, self(), :replace_env_no_agent)
+      end
+    end
+
+    test "updates the agent when it is running" do
+      pid = self()
+      {:ok, agent} = Gestalt.start(:replace_env)
+
+      assert Agent.get(:replace_env, fn state -> state end) == %{}
+      Gestalt.replace_env("REPLACING_VARIABLE", "overridden value", pid, :replace_env)
+      assert Agent.get(:replace_env, fn state -> state end) == %{pid => [env: %{"REPLACING_VARIABLE" => "overridden value"}]}
 
       Agent.stop(agent)
     end
